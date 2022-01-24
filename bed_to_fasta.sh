ee31eaf0056c667c3a92c5bd730d4c33b8aad8e4
Hazel Dilmore <56170641+ahdilmore@users.noreply.github.com>
2022-01-23 18:40:18 -0800

    add bed_to_fasta file

---
+++ bed_to_fasta.sh
@@ -0,0 +1,19 @@
+fasta_dir=../../callaban/Projects/Adaptation/prokka_AZ20/
+genes_of_interest=(garD)
+end=_files/
+end_fa='.fa'
+for gene in "${genes_of_interest[@]}"
+do
+	output_path=$gene$end
+	file_array=($output_path*.bed)
+	
+	for file in "${file_array[@]}"
+	do
+		name="`awk -F'_files/|_Nissle' '{print $2}' <<< "$file"`"
+		fasta_file=$fasta_dir$name/*.fna
+		echo $fasta_file
+		echo $file
+		bedtools getfasta -fo $output_path$name$end_fa -fi $fasta_file -bed $file
+	done
+done
+

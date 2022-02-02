286775f78a1d8895db91f87f3940e905b03addd0
Hazel Dilmore <56170641+ahdilmore@users.noreply.github.com>
2022-02-02 14:02:02 -0800

    formatting changes for bed_to_fasta input

--- csv_to_bed.py
+++ csv_to_bed.py
@@ -2,8 +2,11 @@ import pandas as pd
 
 annotated_genes = pd.read_csv('annotated_genes.csv', 
 			      dtype={'seqname':int, 'start':int,
-				     'end':int})
+				     'end':int, 'db_xref':str,
+				     'eC_number':str})
 genes_of_interest = ['garD']
+final_cols = ['seqname', 'source', 'feature', 'start', 'end',
+	      'score', 'strand', 'frame', 'attribute']
 
 for gene in genes_of_interest:
 	df = annotated_genes.loc[annotated_genes['gene']==gene]
@@ -12,6 +15,6 @@ for gene in genes_of_interest:
 	for file in unique_files: 
 		bed_name = gene + '_files/' + file + '.bed'
 		sub_df = df.loc[df['filename']==file]
-		sub_df = sub_df[['seqname', 'start', 'end']]
+		sub_df = sub_df[final_cols]
 		sub_df.to_csv(bed_name, index=False,
-			      sep='\t')
+			      header=False, sep='\t')

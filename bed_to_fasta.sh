21abc15595309ccdbb27f7af000ff83265c0846e
Hazel Dilmore <56170641+ahdilmore@users.noreply.github.com>
2022-03-22 11:17:12 -0700

    fixed directories!

--- bed_to_fasta.sh
+++ bed_to_fasta.sh
@@ -1,18 +1,21 @@
-fasta_dir=../../callaban/Projects/Adaptation/prokka_AZ20/
-genes_of_interest=(garD)
+fasta_dir=fa_AZ51/
+bed_dir=out_AZ51
+genes_of_interest=($bed_dir/PF*)
 end=_files/
 end_fa='.fa'
 for gene in "${genes_of_interest[@]}"
 do
-	output_path=$gene$end
-	file_array=($output_path*.bed)
-	
+	output_path=$gene/
+	file_array=($gene/*.bed)
 	for file in "${file_array[@]}"
 	do
-		name="`awk -F'_files/|_Nissle' '{print $2}' <<< "$file"`"
-		fasta_file=$fasta_dir$name/*.fna
+		end=${file##*$gene/}
+		name=${end%%_AZ51*}
+		fasta_file=$fasta_dir$name*.fna
 		bedtools getfasta -fo $output_path$name$end_fa -fi $fasta_file -bed $file -name
 	done
 	# merge all files for each gene to end .fa 
+	echo $gene$end_fa
 	cat $output_path*.fa > $gene$end_fa
 done
+echo 'done!'

0c69b3c4e5d81a5f987bce04365b8bb31be5ff68
Hazel Dilmore <56170641+ahdilmore@users.noreply.github.com>
2022-03-28 16:35:17 -0700

    combined bed_to_fasta and hmmalign steps

--- bed_to_fasta.sh
+++ bed_to_fasta.sh
@@ -1,8 +1,11 @@
 fasta_dir=fa_AZ51/
-bed_dir=out_AZ51
-genes_of_interest=($bed_dir/PF*)
-end=_files/
+bed_dir=out_AZ51/
+abbr=PF*
+genes_of_interest=($bed_dir$abbr)
 end_fa='.fa'
+hmm_end=.hmm
+sth_end=.sth
+msa_dir=msa_files/
 for gene in "${genes_of_interest[@]}"
 do
 	output_path=$gene/
@@ -11,10 +14,14 @@ do
 	do
 		x=$(basename $file _AZ51_mut.bed)
 		fasta_file=$fasta_dir$x*.fna
-		bedtools getfasta -fo $output_path$x$end_fa -fi $fasta_file -bed $file -name
+		#bedtools getfasta -fo $output_path$x$end_fa -fi $fasta_file -bed $file -name
 	done
 	# merge all files for each gene to end .fa 
 	echo $gene
 	cat $output_path*.fa > $gene$end_fa
+	# generate hmm file for that pfam and perform msa
+	pfam=$(basename $gene .fa)
+	hmmfetch Pfam-A.hmm $pfam > $bed_dir$msa_dir$pfam$hmm_end 
+	hmmalign -o $bed_dir$msa_dir$pfam$sth_end --trim $bed_dir$msa_dir$pfam$hmm_end $gene$end_fa
 done
 echo 'done!'

89e0c887dc60242724d4d9d0a0f14dc0cd53184c
Hazel Dilmore <56170641+ahdilmore@users.noreply.github.com>
2022-09-12 14:38:16 -0700

    same but for AZ20

--- combine_dfs_to_bed_AZ20.py
+++ combine_dfs_to_bed_AZ20.py
@@ -120,32 +120,47 @@ def get_id(x, str_to_find):
 		return x[:end_index]
 	return x
 
-def make_fasta_name(x):
-	return x['Pfam'] + '_' + x['filename'] + '_' + str(x['rank'])
+def make_fasta_name(x, col_to_sort):
+	return x[col_to_sort] + '_' + x['filename'] + '_' + str(x['rank'])
 
-def make_bed_files(pfam, final_cols, out_path):
-	"""Function to output bed files of all pfam annotated-genes"""
-	pfam.insert(loc=0, column='rank', value=pfam.groupby(['Pfam', 'filename'])['start'].rank().astype('int'))
-	pfam.insert(loc=0, column='name', value=pfam.apply(make_fasta_name, axis=1))
+def make_bed_files(pfam, final_cols, out_path, col_to_sort):
+	"""Function to output bed files of all annotated pfams/genes"""
+	pfam.insert(loc=0, column='rank', value=pfam.groupby([col_to_sort, 'filename'])['start'].rank().astype('int'))
+	pfam.insert(loc=0, column='name', value=pfam.apply(make_fasta_name, args=(col_to_sort, ), axis=1))
 
-	for pfam_id in pfam['Pfam'].unique():
-		df = pfam.loc[pfam['Pfam']==pfam_id]
+	for pfam_id in pfam[col_to_sort].unique():
+		df = pfam.loc[pfam[col_to_sort]==pfam_id]
 		unique_files = df['filename'].unique()
+		print(pfam_id)
 		if not os.path.exists(out_path + pfam_id):
 			os.mkdir(out_path + pfam_id + '/')
 		for f in unique_files:
 			bed_name = out_path + pfam_id + '/' + f + '.bed'
 			sub_df = df.loc[df['filename']==f]
-			print(bed_name)
 			sub_df[final_cols].to_csv(bed_name, index=False, header=False, sep='\t')
 
 
 # combine dfs
-pfam = extract_pfam(files_dir, files_pattern, files_cols, to_remove, attribute_cols)
-pfam.to_csv('pfam_annots_AZ20.csv', index=False)
+#pfam = extract_pfam(files_dir, files_pattern, files_cols, to_remove, attribute_cols)
+#pfam.to_csv('pfam_annots_AZ20.csv', index=False)
 
 # make the bed files
-pfam = pd.read_csv('pfam_annots_AZ20.csv')
-pfam_over_5 = pfam["Pfam"].value_counts()[pfam["Pfam"].value_counts() > 5].index
-pfam = pfam.loc[pfam["Pfam"].isin(pfam_over_5)]
-make_bed_files(pfam, final_cols, out_path) 
+#pfam = pd.read_csv('pfam_annots_AZ20.csv')
+#pfam_over_5 = pfam["Pfam"].value_counts()[pfam["Pfam"].value_counts() > 5].index
+#pfam = pfam.loc[pfam["Pfam"].isin(pfam_over_5)]
+#make_bed_files(pfam, final_cols, out_path)
+
+# FOR GENE COMPARISON: 
+#full_df = concat_annotations(files_dir, files_pattern, files_cols, to_remove)
+#genes_df = full_df.loc[full_df["feature"] == "CDS"]
+#genes_df = split_attribute(genes_df, attribute_cols)
+#genes_df = genes_df.loc[genes_df["gene"].notna()]
+#genes_df.to_csv('AZ20_full_df.csv', index=False)
+
+genes_df = pd.read_csv("AZ20_full_df.csv")
+# genes present must be present in over 100 isolates (~10%)
+genes_over_100 = genes_df["gene"].value_counts()[genes_df["gene"].value_counts() > 100].index
+genes_df = genes_df.loc[genes_df["gene"].isin(genes_over_100)] 
+out_path_new = 'genes_out_AZ20_filt/'
+make_bed_files(genes_df, final_cols, out_path_new, "gene") 
+ 
